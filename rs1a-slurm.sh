1dbad2bd2aa364a762dd9c2e49be5fd889f1b7bf
AWS ParallelCluster user <ec2-user@ip-10-0-9-243.ap-southeast-1.compute.internal>
2026-09-03 12:29:19 +0000

    add new scripts

---
+++ rs1a-slurm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Submit this script with: sbatch <this-filename>
+
+#SBATCH --time=24:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --cpus-per-task=5
+#SBATCH -J "rs1a"   # job name
+## /SBATCH -p general # partition (queue)
+#SBATCH -o %x-slurm.%N.%j.out # STDOUT
+#SBATCH -e %x-slurm.%N.%j.err # STDERR
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+python -u -c "import PyHipp as pyh; \
+import DataProcessingTools as DPT; \
+import time; \
+import os; \
+t0 = time.time(); \
+print(time.localtime()); \
+DPT.objects.processDirs(dirs=None, objtype=pyh.RPLSplit, channel=[*range(1,33)], SkipHPC=False, HPCScriptsDir = '/data/src/PyHipp/', SkipLFP=False, SkipHighPass=False, SkipSort=False); \
+print(time.localtime()); \
+print(time.time()-t0);"
+
+aws sns publish --region ap-southeast-1 --topic-arn arn:aws:sns:ap-southeast-1:221724635492:awsnotify --message "rs1a done"
