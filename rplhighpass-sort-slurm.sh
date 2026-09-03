b46951eb86a987db4f06b35b03429be8b48d6d28
AWS ParallelCluster user <ec2-user@ip-10-0-9-243.ap-southeast-1.compute.internal>
2026-09-03 17:23:09 +0000

    ahhh

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -11,6 +11,11 @@
 #SBATCH -o %x-slurm.%N.%j.out # STDOUT
 #SBATCH -e %x-slurm.%N.%j.err # STDERR
 
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
 import time; \
@@ -19,4 +24,8 @@ from PyHipp import mountain_batch; \
 mountain_batch.mountain_batch(); \
 from PyHipp import export_mountain_cells; \
 export_mountain_cells.export_mountain_cells(); \
-print(time.localtime());"
+print(time.localtime());";
+
+conda deactivate
+/data/src/PyHipp/envlist.py $envarg
+
