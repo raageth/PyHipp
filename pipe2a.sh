eb77d94c7613bf13aacdea8cdd2c63841d59a72e
AWS ParallelCluster user <ec2-user@ip-10-0-9-243.ap-southeast-1.compute.internal>
2026-09-05 07:25:01 +0000

    update pipe2a

--- pipe2a.sh
+++ pipe2a.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# first job called from the day directory
+# creates RPLParallel, Unity, and EDFSplit objects, and
+# calls aligning_objects and raycast
+sbatch /data/src/PyHipp/rplparallel-slurm.sh
+
 # second job - no dependencies, called from the day directory
 jid2=$(sbatch /data/src/PyHipp/rse-slurm.sh)
 
@@ -7,3 +12,4 @@ jid2=$(sbatch /data/src/PyHipp/rse-slurm.sh)
 jid3=$(sbatch --dependency=afterok:${jid2##* } /data/src/PyHipp/rs1a-slurm.sh)
 jid4=$(sbatch --dependency=afterok:${jid2##* } /data/src/PyHipp/rs2a-slurm.sh)
 jid5=$(sbatch --dependency=afterok:${jid2##* } /data/src/PyHipp/rs3a-slurm.sh)
+jid6=$(sbatch --dependency=afterok:${jid2##* } /data/src/PyHipp/rs4a-slurm.sh)
